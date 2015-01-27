9e9fc5d676085c5bd4a4afb1369b0440d5dd4f63
NobuyukiMorii <qwerty.poiu.mory@gmail.com>
2015-01-27 18:17:09 +0800

    add code database_connect select_database cheking_post

--- bbs.php
+++ bbs.php
@@ -1,3 +1,37 @@
+<?php
+
+//データベースに接続
+$link = mysql_connect('localhost' , 'root' , 'camp2015');
+if(!$link) {
+	die('データベースに接続出来ません：' . mysql_error());
+}
+
+//データベースを選択する
+mysql_select_db('online_bbs' , $link);
+$errors = array();
+
+//POSTなら保存処理実行
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+	//名前が正しく入力されているかチェック
+	$name = null;
+	if(!isset($_POST['name']) || !strlen($_POST['name'])){
+		$errors['name'] = '名前を入力して下さい。';
+	} else if(strlen($_POST['name']) > 40) {
+		$errors['name'] = '名前は40文字以内で入力して下さい。';
+	} else {
+		$name = $_POST['name'];
+	}
+}
+
+//
+
+
+
+
+
+
+
+?>
 <!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
